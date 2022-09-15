d2894e5b70fdf2f21d384a135c57093555c36c4a
Jing Zhi <t.jingzhi1@gmail.com>
2022-09-15 18:00:19 +0800

    Added Login

---
+++ views/Login.js
@@ -0,0 +1,12 @@
+import { View } from "react-native";
+import { Text, Box, Button, Heading } from "native-base";
+
+const Login = ({ navigation }) => {
+  return (
+    <View style={{ flex: 1, alignItems: "center" }}>
+      <Heading style={{ paddingTop: 10 }}>Bussin</Heading>
+    </View>
+  );
+};
+
+export default Login;
