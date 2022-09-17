430d37efaf3a31bb46aa339b4022d2bd5adc43ec
jz <t.jingzhi1@gmail.com>
2022-09-17 22:15:28 +0800

    Removed default header

--- views/Login.js
+++ views/Login.js
@@ -1,10 +1,44 @@
 import { View } from "react-native";
-import { Text, Box, Button, Heading } from "native-base";
+import {
+  Text,
+  Box,
+  Button,
+  Heading,
+  FormControl,
+  Input,
+  Stack,
+  Center,
+} from "native-base";
 
 const Login = ({ navigation }) => {
+  const submit = () => {
+    console.log("Submit");
+  };
   return (
     <View style={{ flex: 1, alignItems: "center" }}>
-      <Heading style={{ paddingTop: 10 }}>Bussin</Heading>
+      <Box
+        w="100%"
+        maxWidth="300px"
+        bgColor="muted.50"
+        style={{
+          padding: 30,
+        }}
+      >
+        <Stack mx="10">
+          <Center>
+            <Heading style={{ margin: 10 }}>Bussin</Heading>
+            <FormControl.Label style={{ alignItems: "center" }}>
+              Username
+            </FormControl.Label>
+            <Input type="Text" placeholder="Username" />
+            <FormControl.Label>Password</FormControl.Label>
+            <Input type="password" placeholder="Password" />
+            <Button onPress={submit} w="100%" style={{ marginTop: 30 }}>
+              Sign In
+            </Button>
+          </Center>
+        </Stack>
+      </Box>
     </View>
   );
 };
