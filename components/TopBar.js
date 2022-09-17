3dfe7fc8f959d062c630e4a8e881f3afc586ad11
jz <t.jingzhi1@gmail.com>
2022-09-17 23:47:58 +0800

    Init topbar

---
+++ components/TopBar.js
@@ -0,0 +1,10 @@
+import { View } from "native-base";
+
+/**
+ * The bar on top of the application, together with the status bar.
+ */
+const TopBar = () => {
+  return <View></View>;
+};
+
+export default TopBar;
