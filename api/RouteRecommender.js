c3b207a12134d8cfb5bf35ee130286e77bf4590b
jz <t.jingzhi1@gmail.com>
2022-10-09 04:21:05 +0800

    Added weibin route

---
+++ api/RouteRecommender.js
@@ -0,0 +1,30 @@
+import { BETTER_API_ROUTE } from "@env";
+import ky from "ky";
+
+const useRecommenderAPI = () => {
+  const api = ky.create({
+    prefixUrl: BETTER_API_ROUTE,
+  });
+  const recommend = async (coords) => {
+    console.log(coords);
+    let data = null;
+    try {
+      const res = await api.post("routes", {
+        json: {
+          "Origin Latitude": coords.originLat,
+          "Origin Longitude": coords.originLng,
+          "Destination Latitude": coords.destLat,
+          "Destination Longitude": coords.destLng,
+          "Departure Time": coords.time,
+        },
+      });
+      data = await res.json();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return { recommend };
+};
+
+export { useRecommenderAPI };
