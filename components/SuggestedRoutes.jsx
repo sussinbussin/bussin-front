791d8def7eb55b641204b5f99e8922eb77c878c1
jz <t.jingzhi1@gmail.com>
2022-10-17 15:13:17 +0800

    added carousel

--- components/SuggestedRoutes.jsx
+++ components/SuggestedRoutes.jsx
@@ -1,40 +1,53 @@
-import { Box,FlatList,Text,HStack, ScrollView, Pressable } from "native-base";
-import { StyleSheet } from "react-native";
+import { Box, Heading, Text, Button } from "native-base";
+import { Dimensions, StyleSheet } from "react-native";
 import { GlobalContext } from "../contexts/global";
-
 import { useContext, useEffect } from "react";
-
+import { Carousel } from "react-native-snap-carousel";
 const SuggestedRoutes = () => {
   const {state, dispatch} = useContext(GlobalContext);
+  const {height, width} = Dimensions.get("screen");
 
+  const renderSuggestions = ({item, index}) => {
+    return <Box style={styles.box}>
+      <Heading>{item.carPlate}</Heading>
+      <Text fontSize="md">Capacity: {item.capacity}</Text>
+      <Button variant="outline" onPress={handleSubmit}>Book</Button>
+    </Box>
+  }
+
+  //TODO: handle change map view
+  const handleSnap = (index) => {
+    console.log(index);
+  }
+  //TODO: handle submit request
+  const handleSubmit = () => {
+    
+  }
   useEffect(() => {
     console.log(state.routes);
   }, []);
-    return (<Box style={styles.box}>
-        <Text bold fontSize="lg">Suggested Routes</Text>
-        <ScrollView horizontal={true} style={{
-          width: "100%",
-        }}>
-        {state.routes && state.routes.map(route => <Pressable key={route.id}>
-          <Box style={{
-            width: 300,
-            padding: 15,
-          }}>
-            <Text>Car Plate: {route.carPlate}</Text>
-          </Box>
-        </Pressable>)}
-        </ScrollView>
-    </Box>)
+    return (state.routes && <>
+    <Carousel
+      style={styles.carousel} 
+      data={state.routes}
+      renderItem={renderSuggestions}
+      sliderWidth={width}
+      itemWidth={width * 0.80}
+      onSnapToItem={handleSnap}
+      layout="default"
+    />
+
+    </>
+   )
 };
 
 const styles = StyleSheet.create({
     box: {
-      width: "90%",
       padding: 15,
-      marginBottom: 90,
+      marginBottom: 60,
       borderRadius: 10,
       marginTop: "auto"
-    }
+    },
 })
 export default SuggestedRoutes;
 
