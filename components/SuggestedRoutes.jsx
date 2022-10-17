3d00df52c223a2fda0c0e2e35289c0fdf0efa313
jz <t.jingzhi1@gmail.com>
2022-10-17 17:58:08 +0800

    Added pagination to suggested routes

--- components/SuggestedRoutes.jsx
+++ components/SuggestedRoutes.jsx
@@ -1,12 +1,17 @@
-import { Box, Heading, Text, Button } from "native-base";
+import { Box, Heading, Text, Button, View } from "native-base";
 import { Dimensions, StyleSheet } from "react-native";
 import { GlobalContext } from "../contexts/global";
 import { useContext, useEffect } from "react";
-import { Carousel } from "react-native-snap-carousel";
+import { Carousel, Pagination } from "react-native-snap-carousel";
+import {useState} from 'react';
+
 const SuggestedRoutes = () => {
   const {state, dispatch} = useContext(GlobalContext);
   const {height, width} = Dimensions.get("screen");
 
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  //TODO: implement get req from driver +  skeleton
   const renderSuggestions = ({item, index}) => {
     return <Box style={styles.box}>
       <Heading>{item.carPlate}</Heading>
@@ -15,9 +20,25 @@ const SuggestedRoutes = () => {
     </Box>
   }
 
+  const generatePagination = () => {
+    return state.routes && <Pagination 
+      dotsLength={state.routes.length}
+      activeDotIndex={currentIndex}
+      dotStyle={{
+        width: 8,
+        height: 8,
+        borderRadius: 5,
+        marginHorizontal: 8,
+        backgroundColor: 'rgba(255, 255, 255, 0.92)'
+    }}
+    inactiveDotOpacity={0.4}
+    inactiveDotScale={0.6}
+    />
+  }
+
   //TODO: handle change map view
   const handleSnap = (index) => {
-    console.log(index);
+    setCurrentIndex(index);
   }
   //TODO: handle submit request
   const handleSubmit = () => {
@@ -26,7 +47,9 @@ const SuggestedRoutes = () => {
   useEffect(() => {
     console.log(state.routes);
   }, []);
-    return (state.routes && <>
+
+  //fix bug swipe with whole screen
+  return (state.routes && <>
     <Carousel
       style={styles.carousel} 
       data={state.routes}
@@ -36,7 +59,7 @@ const SuggestedRoutes = () => {
       onSnapToItem={handleSnap}
       layout="default"
     />
-
+    {generatePagination()}
     </>
    )
 };
@@ -44,9 +67,9 @@ const SuggestedRoutes = () => {
 const styles = StyleSheet.create({
     box: {
       padding: 15,
-      marginBottom: 60,
       borderRadius: 10,
-      marginTop: "auto"
+      marginTop: "auto",
+      marginBottom: 60
     },
 })
 export default SuggestedRoutes;
